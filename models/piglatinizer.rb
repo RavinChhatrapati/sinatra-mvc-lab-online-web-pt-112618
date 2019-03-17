78212dbdf01b1c5e5e47c1b5eb708f2b7650cd25
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-17 18:13:40 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,23 +4,23 @@ class PigLatinizer
       letters = word.split("")
       letters.keep_if {|letter| letter != "."}
       if letters.size > 1
-        until vowel?(letters[0]) 
+        until vowel?(letters[0])
           letters << letters.shift
         end
         letters  << "ay"
       end
       letters.join
     end
-  
+
      def to_pig_latin(text)
       words = text.split(" ")
       words.map! {|word| piglatinize(word)}
       words.join(" ")
     end
-  
+
      def vowel?(letter)
       letter.downcase
       letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
     end
-  
+
    end 
